eadfdc3814d0a267112a04f90aee9253ebb305c5
Mohamed Amir <m.elamir027@gmail.com>
2025-01-15 23:12:39 +0200

    fix(authentication): remove unused import for audit log

--- packages/server/api/src/app/authentication/authentication.controller.ts
+++ packages/server/api/src/app/authentication/authentication.controller.ts
@@ -1,32 +1,17 @@
-import { AppSystemProp, system } from '@pickup/server-shared'
-import {
-    ALL_PRINCIPAL_TYPES,
-    SignInRequest,
-    ApplicationEventName,
-} from '@pickup/shared'
-import { RateLimitOptions } from '@fastify/rate-limit'
-import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox'
-import { eventsHooks } from '../helper/application-events'
-import { authenticationService } from './authentication-service'
+import {AppSystemProp, system} from '@pickup/server-shared'
+import {ALL_PRINCIPAL_TYPES, SignInRequest,} from '@pickup/shared'
+import {RateLimitOptions} from '@fastify/rate-limit'
+import {FastifyPluginAsyncTypebox} from '@fastify/type-provider-typebox'
+import {authenticationService} from './authentication-service'
 
 export const authenticationController: FastifyPluginAsyncTypebox = async (
     app,
 ) => {
     app.post('/sign-in', SignInRequestOptions, async (request) => {
-        const response = await authenticationService.signIn({
+        return await authenticationService.signIn({
             email: request.body.email,
             password: request.body.password,
-        });
-
-        eventsHooks.get().sendUserEvent({
-            userId: response.id,
-            ip: request.ip,
-        }, {
-            action: ApplicationEventName.USER_SIGNED_IN,
-            data: {},
         })
-
-        return response
     })
 }
 
