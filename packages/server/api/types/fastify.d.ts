a62b75b4e439bca67219e4c9dd821a17e41e529f
Mohamed Amir <m.elamir027@gmail.com>
2025-01-21 15:15:46 +0200

    fix: fix classroomId with temp solution

--- packages/server/api/types/fastify.d.ts
+++ packages/server/api/types/fastify.d.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { EndpointScope, Permission, Principal, PrincipalType } from '@pickup/shared'
+import { EndpointScope, Permission, Principal, PrincipalType } from '@activepieces/shared'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import fastify from 'fastify'
 
